523f9344548088b9075342d32248da386bab37b9
Jzheng-prog <74681328+Jzheng-prog@users.noreply.github.com>
2024-11-12 03:33:51 -0500

    onboarding page

--- app/index.jsx
+++ app/index.jsx
@@ -1,15 +1,16 @@
 import {Image, Text, View, ScrollView } from 'react-native'
 import React from 'react'
-import { Link } from 'expo-router'
-import { SafeAreaView } from 'react-native-safe-area-context'
+import {router} from 'expo-router'
+import {SafeAreaView } from 'react-native-safe-area-context'
 import {images} from '../constants'
 import CustomButton from '../components/CustomButton'
+import ExpoStatusBar from 'expo-status-bar/build/ExpoStatusBar'
 
 export default function App(){
   return (
     <SafeAreaView className='bg-primary h-full'>
       <ScrollView contentContainerStyle={{height:'100%'}}>
-        <View className='w-full justify-center items-center h-full px-4'>
+        <View className='w-full justify-center items-center h-full px-4 border border-white'>
           <Image source={images.logo} className='w-[130px] h-[84px]' resizeMode='contain'/> 
           <Image source={images.cards} className='w-full h-[300px] max-w-[380px]' resizeMode='contain'/> 
 
@@ -22,11 +23,13 @@ export default function App(){
           </View>
           <Text className='text-sm font-pregular text-gray-100 mt-7 text-center'>Where creativity meets innovation: embark on a journey of limitless exploration with Aora</Text>
           <CustomButton 
-            text={'Continue with Email'} 
-            handlePress={()=>{}}
-            containerStyle='w-full mt-7'/>
+            text='Continue with Email'
+            handlePress={()=> router.push('/sign-in')}
+            containerStyle='w-full mt-7'
+            />
         </View>
       </ScrollView>
+      <ExpoStatusBar backgroundColor='#161622' style='light'/>
     </SafeAreaView>
   )
 }
--- components/CustomButton.jsx
+++ components/CustomButton.jsx
@@ -2,12 +2,13 @@ import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import { TouchableOpacity } from 'react-native'
 
-const CustomButton = ({text, handlePress, containerStyles, textStyles, isLoading}) => {
+const CustomButton = ({text, handlePress, containerStyle, textStyles, isLoading}) => {
   return (
     <TouchableOpacity 
-        className={`${containerStyles}bg-secondary rounded-xl min-h-[62px] justify-center items-center ${isLoading?'opacity-50':''}`}
+        className={` bg-secondary rounded-xl min-h-[62px] justify-center items-center ${isLoading?'opacity-50':''} ${containerStyle}`}
         disabled={isLoading}
-        onPress={handlePress}activeOpacity={0.7}
+        onPress={handlePress}
+        activeOpacity={0.7}
     >
         <Text className={`text-primary font-psemibold text-lg${textStyles}`}>{text}</Text>
     </TouchableOpacity>
