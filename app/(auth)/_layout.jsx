a43a23530cc3eada247cbd2095c256ee6bce294d
Jzheng-prog <74681328+Jzheng-prog@users.noreply.github.com>
2024-11-12 21:00:30 -0500

    laptop

--- app/(auth)/_layout.jsx
+++ app/(auth)/_layout.jsx
@@ -1,14 +1,17 @@
-import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { Stack } from 'expo-router'
+import ExpoStatusBar from 'expo-status-bar/build/ExpoStatusBar'
 
 const Authlayout = () => {
   return (
-    <View>
-      <Text>-layout</Text>
-    </View>
+    <>
+      <Stack>
+        <Stack.Screen name='sign-in' options={{headerShown:false}}/>
+        <Stack.Screen name='sign-out' options={{headerShown:true}}/>
+      </Stack>
+      <ExpoStatusBar backgroundColor='#161622' style='dark'/>
+    </>
   )
 }
 
 export default Authlayout
-
-const styles = StyleSheet.create({})
--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -1,11 +1,39 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-
+import { StyleSheet, Text, View, ScrollView,Image } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import {images} from '../../constants'
+import FormField from '../../components/FormField'
+import { useState } from 'react'
 const SignIn = () => {
+  const [form, setForm] = useState({
+    email:'',
+    password:''
+  })
   return (
-    <View>
-      <Text>sign-in</Text>
-    </View>
+    <SafeAreaView className='bg-primary h-full'>
+      <ScrollView>
+        <View className='w-full justify-center h-full px-4 my-6'>
+          <Image source={images.logo} resizeMode='contain' className='w-[115px] h-[35px]'/>
+          <Text className='text-white text-semibold text-2xl mt-10 font-psemibold'>Log in to Aora</Text>
+          <FormField
+          title='Email'
+          value={form.email}
+          handleChangeText={
+            (e)=>setForm({...form, email: e})
+          }
+          keyboardType='email-address'
+          otherStyles='mt-7'
+          />
+          <FormField
+          title='Password'
+          value={form.password}
+          handleChangeText={
+            (e)=>setForm({...form, password: e})
+          }
+          otherStyles='mt-7'
+          />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   )
 }
 
